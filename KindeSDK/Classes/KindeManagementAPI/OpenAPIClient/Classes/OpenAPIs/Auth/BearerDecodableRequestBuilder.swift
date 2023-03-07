f6bf8487f6fa1af26569d635b32dad1636761a01
Tanzilya <Yakshimbetova.tanzilya@gmail.com>
2023-03-07 12:04:39 +0100

    Update BearerDecodableRequestBuilder.swift

--- KindeSDK/Classes/KindeManagementAPI/OpenAPIClient/Classes/OpenAPIs/Auth/BearerDecodableRequestBuilder.swift
+++ KindeSDK/Classes/KindeManagementAPI/OpenAPIClient/Classes/OpenAPIs/Auth/BearerDecodableRequestBuilder.swift
@@ -1,18 +1,15 @@
-class BearerDecodableRequestBuilder<T: Decodable>: URLSessionDecodableRequestBuilder<T> {    
+class BearerDecodableRequestBuilder<T: Decodable>: URLSessionDecodableRequestBuilder<T> {
     @discardableResult
-    override func execute(_ apiResponseQueue: DispatchQueue = OpenAPIClientAPI.apiResponseQueue, _ completion: @escaping (Result<Response<T>, ErrorResponse>) -> Void) async -> RequestTask {
+    override func execute(_ apiResponseQueue: DispatchQueue = OpenAPIClientAPI.apiResponseQueue, _ completion: @escaping (Result<Response<T>, ErrorResponse>) -> Void) -> RequestTask {
         let bearerTokenHandler = BearerTokenHandler()
-        do {
-            try await bearerTokenHandler.setBearerToken()
-            super.execute(apiResponseQueue) { result in
-                completion(result)
-            }
-        } catch let error {
-            do {
-                let result: Response<T> = try await bearerTokenHandler.handleSetBearerTokenError(error: error)
-                completion(.success(result))
-            } catch {
-                completion(.failure(error as! ErrorResponse))
+        bearerTokenHandler.setBearerToken { error in
+
+            if let error = error {
+                bearerTokenHandler.handleSetBearerTokenError(error: error, completion: completion)
+            } else {
+                super.execute(apiResponseQueue) { result in
+                    completion(result)
+                }
             }
         }
         
