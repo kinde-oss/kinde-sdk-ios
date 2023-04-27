b629d548eb1e9bdabfe181c942494ae550b75658
Tanzilya <Yakshimbetova.tanzilya@gmail.com>
2023-04-27 22:25:03 +0200

    - update tests

---
+++ Tests/AuthSpec.swift
@@ -0,0 +1,43 @@
+import Quick
+import Nimble
+import KindeSDK
+
+class AuthSpec: QuickSpec {
+    override func spec() {        
+        describe("Auth") {
+            it("is unauthorised after initialisation") {
+                KindeSDKAPI.configure()
+                expect(KindeSDKAPI.auth.isAuthorized()) == false
+            }
+            
+            it("check helper functions") {
+                let auth: Auth = KindeSDKAPI.auth
+                guard auth.isAuthorized() == true else { return }
+                let userDetails: User? = auth.getUserDetails()
+                expect(userDetails?.id).to(beGreaterThan(0))
+                
+                let audClaim = auth.getClaim(key: "aud")
+                expect(audClaim).notTo(beNil())
+                
+                let permissions = auth.getPermissions()
+                expect(permissions).notTo(beNil())
+                
+                let organization = auth.getOrganization()
+                expect(organization).notTo(beNil())
+                
+                let userOrganizations = auth.getUserOrganizations()
+                expect(userOrganizations).notTo(beNil())
+            }
+            
+            it("check logout functions") {
+                let auth: Auth = KindeSDKAPI.auth
+                guard auth.isAuthorized() == true else { return }
+
+                let result = await auth.logout()
+                if result == true {
+                    expect(auth.isAuthorized()).to(beFalse())
+                }                
+            }
+        }
+    }
+}
